b7d254f69f2d7c2b530d28f5573127431641de93
Shashankcognic <shashank.patel@cognicsys.com>
2023-04-08 18:45:57 +0530

    second release

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
+import Header from "./Header";
 
 const Layout = () => {
   return (
     <div className="flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden">
       <Sidebar />
-      <div className="p-4">
-        <div>Header</div>
-        <div>{<Outlet />}</div>
+      <div className="flex flex-col flex-1">
+        <Header />
+        <div className="flex-1  min-h-0 overflow-auto">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
